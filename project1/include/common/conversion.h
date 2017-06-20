bfa05c7cbaf8136acfa463518c4955437701c20a
Vivek Sankaranarayanan <vivek.sankaranarayanan@colorado.edu>
2017-06-20 02:45:25 -0600

    little_to_big32 added

--- project1/include/common/conversion.h
+++ project1/include/common/conversion.h
@@ -13,6 +13,23 @@
 
 #include <stdint.h>
 
+/*---------------------------------------------------------------------------*/
+/* ---- Bitwise operations --------- */
+#define LSHIFT_BYTE(a)		(a) << 8
+#define LSHIFT_2_BYTES(a)	(a) << 16
+#define LSHIFT_3_BYTES(a)	(a) << 24
+
+#define RSHIFT_BYTE(a)		(a) >> 8
+#define RSHIFT_2_BYTES(a)	(a) >> 16
+#define RSHIFT_3_BYTES(a)	(a) >> 24
+
+#define	BYTE0(a)			(a) & 0xFF
+#define	BYTE1(a)			(a) & 0xFF00
+#define	BYTE2(a)			(a) & 0xFF0000
+#define	BYTE3(a)			(a) & 0xFF000000
+
+/*---------------------------------------------------------------------------*/
+/* ---- Function Prototypes --------- */
 uint8_t my_itoa(int32_t data, uint8_t *ptr, uint32_t base);
 int32_t my_atoi(uint8_t *ptr, uint8_t digits, uint32_t base);
 int8_t big_to_little32(uint32_t * data, uint32_t length);
--- project1/src/conversion.c
+++ project1/src/conversion.c
@@ -20,20 +20,50 @@ uint8_t my_itoa(int32_t data, uint8_t *ptr, uint32_t base)
 	return 0;
 }
 
-/**
- * 
- */
 int32_t my_atoi(uint8_t *ptr, uint8_t digits, uint32_t base)
 {
 	return 0;
 }
 
-int8_t big_to_little32(uint32_t * data, uint32_t length)
+int8_t big_to_little32(uint32_t *data, uint32_t length)
 {
 	return 0;
 }
 
-int8_t little_to_big32(uint32_t * data, uint32_t length)
-{
-  return 0;
-}
+
+/*---------------------------------------------------------------------------*/
+/**
+ * @brief The function little_to_big32 takes an array and size as
+ *        inputs, and converts all words from little to big endian.
+ *
+ *        Little to big endian for a 32bit word conversion happens as follows:
+ *        BYTE3 => BYTE0
+ *        BYTE2 => BYTE1
+ *        BYTE1 => BYTE2
+ *        BYTE0 => BYTE3
+ * 
+ * @param data - array that needs to be converted
+ * @param length - size of the array
+ *
+ * @return Returns SUCCESS if the array was successfully converted else returns
+ *         FAILURE.
+ */
+int8_t little_to_big32(uint32_t *data, uint32_t length)
+  {
+    int8_t result;
+    
+    if(data == NULL || length <= 0)
+      {
+        result = FAILURE;
+      }
+    else
+      {
+        for (uint32_t *ptr1 = src, *end = src + length; ptr1 < end; ) 
+          {
+          *ptr1 = RSHIFT_3_BYTES(*ptr1) | BYTE1(RSHIFT_BYTE(*ptr1)) | 
+                  BYTE2(LSHIFT_BYTE(*ptr1)) | BYTE3(LSHIFT_3_BYTES(*ptr1));
+          }
+        result = SUCCESS;
+      }
+    return result;  
+  }
