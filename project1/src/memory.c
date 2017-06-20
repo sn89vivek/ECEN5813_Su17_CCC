54731855f3c4094c38075953d9f7c154057200a6
Robert Blazewicz <blaze@cardoduplex.com>
2017-06-19 23:15:46 -0400

    Adjust paren

--- project1/src/memory.c
+++ project1/src/memory.c
@@ -30,20 +30,24 @@
  * @return Pointer to the destination on success
  */
 uint8_t *my_memcpy(uint8_t *src, uint8_t *dst, size_t length)
-{
+  {
   uint8_t *result;
 
-  if (NULL == src || NULL == dst || length <= 0) {
+  if (NULL == src || NULL == dst || length <= 0)
+    {
     result = NULL;
-  } else {
-    for (uint8_t *ptr1 = src, *ptr2 = dst, *end = src + length; ptr1 < end; ) {
-      *ptr2++ = *ptr1++;
     }
+  else
+    {
+    for (uint8_t *ptr1 = src, *ptr2 = dst, *end = src + length; ptr1 < end; )
+      {
+      *ptr2++ = *ptr1++;
+      }
     result = dst;
-  }
+    }
 
   return result;
-}
+  }
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -57,9 +61,9 @@ uint8_t *my_memcpy(uint8_t *src, uint8_t *dst, size_t length)
  * @return xxx
  */
 int8_t *my_memset(uint8_t *src, size_t length, uint8_t value)
-{
+  {
   return NULL;
-}
+  }
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -73,20 +77,24 @@ int8_t *my_memset(uint8_t *src, size_t length, uint8_t value)
  * @return Pointer to source
  */
 uint8_t *my_memzero(uint8_t *src, size_t length)
-{
+  {
   uint8_t *result;
 
-  if (NULL == src || length <= 0) {
+  if (NULL == src || length <= 0)
+    {
     result = NULL;
-  } else {
-    for (uint8_t *ptr1 = src, *end = src + length; ptr1 < end; ) {
-      *ptr1++ = 0x00;
     }
+  else
+    {
+    for (uint8_t *ptr1 = src, *end = src + length; ptr1 < end; )
+      {
+      *ptr1++ = 0x00;
+      }
     result = src;
-  }
+    }
 
   return result;
-}
+  }
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -99,9 +107,9 @@ uint8_t *my_memzero(uint8_t *src, size_t length)
  * @return xxx
  */
 uint8_t *my_reverse(uint8_t *src, size_t length)
-{
+  {
   return NULL;
-}
+  }
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -114,18 +122,21 @@ uint8_t *my_reverse(uint8_t *src, size_t length)
  * @return Pointer to allocated memory
  */
 int32_t *reserve_words(size_t length)
-{
+  {
   uint8_t *result;
 
-  if (length <= 0) {
+  if (length <= 0)
+    {
     result = NULL;
-  } else {
+    }
+  else
+    {
     const size_t malloc_bytes = length * sizeof(unit16_t);
     result = (uint32_t*)malloc(malloc_bytes);
-  }
+    }
 
   return result;
-}
+  }
 
 /*---------------------------------------------------------------------------*/
 /**
@@ -136,6 +147,6 @@ int32_t *reserve_words(size_t length)
  * @return xxx
  */
 void free_words(uint32_t *src)
-{
+  {
 
-}
+  }
