af32623afaf4b6e4d5e349fa47324ebb65ec9e4e
Robert Blazewicz <blaze@cardoduplex.com>
2017-06-20 19:34:02 -0400

    Add header file for common definitions used by our team

---
+++ project1/include/common/common_ccc.h
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------------------
+ * ECEN-5813 Summer 2017
+ * Project 1
+ *
+ * Copyright (C) 2017 by Robert Blazewicz and Vivek Sankaranarayanan.
+ * All Rights Reserved. (This work is unpublished)
+ *---------------------------------------------------------------------------*/
+/**
+ * @file common_ccc.c
+ * @author Robert Blazewicz
+ * @author Vivek Sankaranarayanan
+ * @date June 20, 2017
+ * @brief Common defintions for use by the Crazy Crafty Coders.
+ */
+
+#ifndef __COMMON_CCC_H__
+#define __COMMON_CCC_H__
+
+/*---------------------------------------------------------------------------*/
+
+#define SUCCESS (0)
+#define FAILURE (1)
+
+typedef char char8_t;
+
+/*---------------------------------------------------------------------------*/
+
+#endif /* __COMMON_CCC_H__ */
