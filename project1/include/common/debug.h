983c67996d09b7460594cf07ba9f1d088a2d532d
Robert Blazewicz <blaze@cardoduplex.com>
2017-06-21 21:03:20 -0400

    Move function comments from .c to .h for debug

--- project1/include/common/debug.h
+++ project1/include/common/debug.h
@@ -1,18 +1,37 @@
+/*-----------------------------------------------------------------------------
+ * ECEN-5813 Summer 2017
+ * Project 1
+ *
+ * Copyright (C) 2017 by Robert Blazewicz and Vivek Sankaranarayanan.
+ * All Rights Reserved. (This work is unpublished)
+ *---------------------------------------------------------------------------*/
 /**
- * @file debug.h
- * @brief Macros and prototypes for debugging related functions
- *
- * TODO
- *
- * @author Robert Blazewicz/Vivek Sankaranarayanan
+ * @file debug.c
+ * @author Robert Blazewicz
+ * @author Vivek Sankaranarayanan
  * @date June 19, 2017
+ * @brief Declarations for debugging related functions.
  */
 
 #ifndef __DEBUG_H__
 #define __DEBUG_H__
 
+/*---------------------------------------------------------------------------*/
+
 #include <stdint.h>
 
+/**
+ * @brief The function print_memory() takes a pointer to memory and prints the
+ *        hex output of bytes given a pointer to a memory location and a length
+ *        of bytes to print.
+ *
+ * @param  src - Start address of memory
+ * @param  length - Number of bytes to print
+ *
+ * @return None
+ */
 void print_memory(uint8_t *start, uint32_t length);
 
-#endif
+/*---------------------------------------------------------------------------*/
+
+#endif /* __DEBUG_H__ */
--- project1/src/debug.c
+++ project1/src/debug.c
@@ -18,33 +18,18 @@
 #include "common_ccc.h"
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function print_memory() takes a pointer to memory and prints the
- *        hex output of bytes given a pointer to a memory location and a length
- *        of bytes to print.
- *
- * @param  src - Start address of memory
- * @param  length - Number of bytes to print
- *
- * @return None
- */
+
 void print_memory(uint8_t *start, uint32_t length)
   {
-  const char8_t hex[16] =
-    {
-    '0', '1', '2', '3', '4', '5', '6', '7',
-    '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-    };
-
   if (start != NULL && length > 0) 
     {
     size_t ctr = 0;
     for (uint8_t *ptr = start, *end = start + length; ptr < end; )
       {
-      const int value1 = hex[*ptr >> 4];
-      const int value2 = hex[*ptr & 0x0F];
-      (void)fputc(value1, stderr);
-      (void)fputc(value2, stderr);
+      const int value1 = *ptr >> 4;
+      const int value2 = *ptr & 0x0F;
+      (void)fputc(value1 <= 9 ? '0' + value1 : 'A' + value1, stderr);
+      (void)fputc(value2 <= 9 ? '0' + value2 : 'A' + value2, stderr);
       if (0x03 == (ctr++ & 0x03))
         {
         (void)fputc('.', stderr);
