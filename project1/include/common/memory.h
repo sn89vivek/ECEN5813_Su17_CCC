47f6950eee40495ec69195414c6b654f61c15ae4
Robert Blazewicz <blaze@cardoduplex.com>
2017-06-21 20:57:02 -0400

    Move function comments from .c to .h for memory

--- project1/include/common/memory.h
+++ project1/include/common/memory.h
@@ -1,24 +1,116 @@
+/*-----------------------------------------------------------------------------
+ * ECEN-5813 Summer 2017
+ * Project 1
+ *
+ * Copyright (C) 2017 by Robert Blazewicz and Vivek Sankaranarayanan.
+ * All Rights Reserved. (This work is unpublished)
+ *---------------------------------------------------------------------------*/
 /**
  * @file memory.h
- * @brief Memory manipulation functions (header file)
- *
- * TODO
- *
- * @author Robert Blazewicz/Vivek Sankaranarayanan
+ * @author Robert Blazewicz
+ * @author Vivek Sankaranarayanan
  * @date June 19, 2017
+ * @brief This file contains declaration of memory manipulation functions.
  */
 
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
+/*---------------------------------------------------------------------------*/
+
 #include <stdint.h>
 
+/**
+ * @brief The function my_memmove() takes two byte pointers (one source and
+ *        one destination) and a length of bytes to copy from the source
+ *        location to the destination.
+ *
+ * @note This function does not handles overlap of src and dst. Will exit copy
+ *       if src and dst overlap.
+ *
+ * @param  src - Source address
+ * @param  dst - Destination address
+ * @param  length - Number of bytes to copy
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to the destination on success
+ */
 uint8_t * my_memmove(uint8_t *src, uint8_t *dst, size_t length);
+
+/**
+ * @brief The function my_memcpy() takes two byte pointers (one source and
+ *        one destination) and a length of bytes to copy from the source
+ *        location to the destination.
+ *
+ * @warning This function does not handle overlap of src and dst. Data can get
+ *          corrupted if overlap detected.
+ * @param  src - Source address
+ * @param  dst - Destination address
+ * @param  length - Number of bytes to copy
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to the destination on success
+ */
 uint8_t * my_memcpy(uint8_t *src, uint8_t *dst, size_t length);
+
+/**
+ * @brief  The function my_memset() takes a pointer to a memory location, a
+ *         length in bytes, a value and sets the memory to the specified value.
+ *
+ * @param  src - Source address of set operation
+ * @param  length - Number of bytes to set
+ * @param  value - Value to set the memory to
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to source
+ */
 uint8_t * my_memset(uint8_t *src, size_t length, uint8_t value);
+
+/**
+ * @brief The function my_memzero() takes a pointer to a memory location, a
+ *        length in bytes and zeros out all of the memory.
+ *
+ * @param  src - Source address of set operation
+ * @param  length - Number of bytes to zero out
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to source
+ */
 uint8_t * my_memzero(uint8_t *src, size_t length);
+
+/**
+ * @brief The function my_reverse() takes a pointer to a memory location, a
+ *        length in bytes and reverses the bytes in the specified memory range.
+ *
+ * @param  src - Source address for the reverse operation
+ * @param  length - Number of bytes to reverse
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to source
+ */
 uint8_t * my_reverse(uint8_t * src, size_t length);
+
+/**
+ * @brief The function reserve_words() takes a length of words to allocate in
+ *        dynamic memory.
+ *
+ * @param  length - Number of words to allocate
+ *
+ * @return NULL value indicates error condition
+ * @return Pointer to allocated memory
+ */
 int32_t * reserve_words(size_t length);
+
+/**
+ * @brief The function free_words() takes a length of words to allocate in
+ *        dynamic memory.
+ *
+ * @param  src - Source address of allocated memory
+ *
+ * @return None
+ */
 void free_words(uint32_t * src);
 
-#endif
+/*---------------------------------------------------------------------------*/
+
+#endif /* __MEMORY_H__ */
--- project1/src/memory.c
+++ project1/src/memory.c
@@ -18,21 +18,7 @@
 #include "common_ccc.h"
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function my_memmove() takes two byte pointers (one source and
- *        one destination) and a length of bytes to copy from the source
- *        location to the destination.
- *
- * @note This function does not handles overlap of src and dst. Will exit copy
- *       if src and dst overlap.
- *
- * @param  src - Source address
- * @param  dst - Destination address
- * @param  length - Number of bytes to copy
- *
- * @return NULL value indicates error condition
- * @return Pointer to the destination on success
- */
+
 uint8_t *my_memmove(uint8_t *src, uint8_t *dst, size_t length)
   {
   uint8_t *result;
@@ -57,20 +43,7 @@ uint8_t *my_memmove(uint8_t *src, uint8_t *dst, size_t length)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function my_memcpy() takes two byte pointers (one source and
- *        one destination) and a length of bytes to copy from the source
- *        location to the destination.
- *
- * @warning This function does not handle overlap of src and dst. Data can get
- *          corrupted if overlap detected.
- * @param  src - Source address
- * @param  dst - Destination address
- * @param  length - Number of bytes to copy
- *
- * @return NULL value indicates error condition
- * @return Pointer to the destination on success
- */
+
 uint8_t *my_memcpy(uint8_t *src, uint8_t *dst, size_t length)
   {
   uint8_t *result;
@@ -92,17 +65,7 @@ uint8_t *my_memcpy(uint8_t *src, uint8_t *dst, size_t length)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief  The function my_memset() takes a pointer to a memory location, a
- *         length in bytes, a value and sets the memory to the specified value.
- *
- * @param  src - Source address of set operation
- * @param  length - Number of bytes to set
- * @param  value - Value to set the memory to
- *
- * @return NULL value indicates error condition
- * @return Pointer to source
- */
+
 uint8_t *my_memset(uint8_t *src, size_t length, uint8_t value)
   {
   uint8_t *result;
@@ -124,16 +87,7 @@ uint8_t *my_memset(uint8_t *src, size_t length, uint8_t value)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function my_memzero() takes a pointer to a memory location, a
- *        length in bytes and zeros out all of the memory.
- *
- * @param  src - Source address of set operation
- * @param  length - Number of bytes to zero out
- *
- * @return NULL value indicates error condition
- * @return Pointer to source
- */
+
 uint8_t *my_memzero(uint8_t *src, size_t length)
   {
   uint8_t *result;
@@ -155,20 +109,11 @@ uint8_t *my_memzero(uint8_t *src, size_t length)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function my_reverse() takes a pointer to a memory location, a
- *        length in bytes and reverses the bytes in the specified memory range.
- *
- * @param  src - Source address for the reverse operation
- * @param  length - Number of bytes to reverse
- *
- * @return NULL value indicates error condition
- * @return Pointer to source
- */
+
 uint8_t *my_reverse(uint8_t *src, size_t length)
   {
   uint8_t *result;
-  uint8_t swap;
+  uint8_t  swap;
 
   if (NULL == src || length <= 0) 
     {
@@ -176,7 +121,7 @@ uint8_t *my_reverse(uint8_t *src, size_t length)
     } 
   else 
     {
-    for (uint8_t *ptr = src, *end = src + (length-1); ptr < end; ) 
+    for (uint8_t *ptr = src, *end = src + (length - 1); ptr < end; )
       {
       swap = *ptr;
       *ptr++ = *end;
@@ -189,15 +134,7 @@ uint8_t *my_reverse(uint8_t *src, size_t length)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function reserve_words() takes a length of words to allocate in
- *        dynamic memory.
- *
- * @param  length - Number of words to allocate
- *
- * @return NULL value indicates error condition
- * @return Pointer to allocated memory
- */
+
 int32_t *reserve_words(size_t length)
   {
   int32_t *result;
@@ -216,14 +153,7 @@ int32_t *reserve_words(size_t length)
   }
 
 /*---------------------------------------------------------------------------*/
-/**
- * @brief The function free_words() takes a length of words to allocate in
- *        dynamic memory.
- *
- * @param  src - Source address of allocated memory
- *
- * @return None
- */
+
 void free_words(uint32_t *src)
   {
   if (src != NULL)
