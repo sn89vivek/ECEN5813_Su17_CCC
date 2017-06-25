edccbe6780e03ef6f5beaf71570eccb731e0e57d
Robert Blazewicz <blaze@cardoduplex.com>
2017-06-24 17:32:39 -0700

    Increment ptr bug

--- project1/src/debug.c
+++ project1/src/debug.c
@@ -24,16 +24,21 @@ void print_memory(uint8_t *start, uint32_t length)
   if (start != NULL && length > 0) 
     {
     size_t ctr = 0;
-    for (uint8_t *ptr = start, *end = start + length; ptr < end; )
+    for (uint8_t *ptr = start, *end = start + length; ptr < end; ptr++)
       {
+      /* Add a period between every 4 bytes */
+      if ((0x00 == (ctr++ & 0x03)) && (ptr > start))
+        {
+        (void)fputc('.', stderr);
+        }
+
+      /* Convert each byte to hexadecimal and output to stderr */
       const int value1 = *ptr >> 4;
       const int value2 = *ptr & 0x0F;
       (void)fputc(value1 <= 9 ? '0' + value1 : 'A' + (value1-10), stderr);
       (void)fputc(value2 <= 9 ? '0' + value2 : 'A' + (value2-10), stderr);
-      if (0x03 == (ctr++ & 0x03))
-        {
-        (void)fputc('.', stderr);
-        }
       }
     }
+  (void)fputc(LF, stderr);
+  (void)fputc(CR, stderr);
   }
