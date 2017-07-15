3b8a9dc9239462297d56e59bdfbec0f86a07687f
Robert Blazewicz <blaze@cardoduplex.com>
2017-07-15 19:36:06 -0400

    Add comments to conversion tests

--- project2/src/conversion_test.c
+++ project2/src/conversion_test.c
@@ -28,11 +28,26 @@
 
 /*---------------------------------------------------------------------------*/
 
+/**
+ * @brief For function my_itoa(), validate arg 'ptr' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_itoa__1(void **state)
   {
   assert_int_equal(my_itoa(0, NULL, 2), 0);
   }
 
+/**
+ * @brief For function my_itoa(), validate arg 'base' with invalid low value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_itoa__2(void **state)
   {
   uint8_t *buffer = (uint8_t*)alloca(BUFFER_SIZE);
@@ -40,6 +55,13 @@ void my_itoa__2(void **state)
   assert_int_equal(my_itoa(0, buffer, 1), 0);
   }
 
+/**
+ * @brief For function my_itoa(), validate arg 'base' with invalid high value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_itoa__3(void **state)
   {
   uint8_t *buffer = (uint8_t*)alloca(BUFFER_SIZE);
@@ -47,30 +69,59 @@ void my_itoa__3(void **state)
   assert_int_equal(my_itoa(0, buffer, 17), 0);
   }
 
+/**
+ * @brief For function my_itoa(), validate a base 10 conversion.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_itoa__4(void **state)
   {
   uint8_t *buffer = (uint8_t*)alloca(BUFFER_SIZE);
   assert_non_null(buffer);
   assert_int_equal(my_itoa(-4096, buffer, 16), 5);
-  assert_int_equal(strcmp((const char8_t*)buffer, "-1000"), 0);
+  assert_string_equal((const char8_t*)buffer, "-1000");
   }
 
+/**
+ * @brief For function my_itoa(), validate base 16 conversion.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_itoa__5(void **state)
   {
   uint8_t *buffer = (uint8_t*)alloca(BUFFER_SIZE);
   assert_non_null(buffer);
   assert_int_equal(my_itoa(123456, buffer, 10), 6);
-  assert_int_equal(strcmp((const char8_t*)buffer, "123456"), 0);
+  assert_string_equal((const char8_t*)buffer, "123456");
   }
 
 /*---------------------------------------------------------------------------*/
 
+/**
+ * @brief For function my_atoi(), validate arg 'ptr' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__1(void **state)
   {
   int32_t data;
   assert_int_equal(my_atoi(NULL, 1, 2, &data), FAILURE);
   }
 
+/**
+ * @brief For function my_atoi(), validate arg 'digits' with invalid value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__2(void **state)
   {
   int32_t data;
@@ -78,6 +129,13 @@ void my_atoi__2(void **state)
   assert_int_equal(my_atoi((uint8_t*)buffer, 0, 2, &data), FAILURE);
   }
 
+/**
+ * @brief For function my_atoi(), validate arg 'digits' with invalid low value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__3(void **state)
   {
   int32_t data;
@@ -85,6 +143,14 @@ void my_atoi__3(void **state)
   assert_int_equal(my_atoi((uint8_t*)buffer, 1, 1, &data), FAILURE);
   }
 
+/**
+ * @brief For function my_atoi(), validate arg 'digits' with invalid high
+ *        value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__4(void **state)
   {
   int32_t data;
@@ -92,12 +158,28 @@ void my_atoi__4(void **state)
   assert_int_equal(my_atoi((uint8_t*)buffer, 1, 17, &data), FAILURE);
   }
 
+/**
+ * @brief For function my_atoi(), validate arg 'data' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__5(void **state)
   {
   char8_t buffer[] = "";
   assert_int_equal(my_atoi((uint8_t*)buffer, 1, 2, NULL), FAILURE);
   }
 
+/**
+ * @brief For function my_atoi(), validate failure of negative sign with no
+ *        digits.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__6(void **state)
   {
   int32_t data;
@@ -105,14 +187,28 @@ void my_atoi__6(void **state)
   assert_int_equal(my_atoi((uint8_t*)buffer, 1, 10, &data), FAILURE);
   }
 
-
+/**
+ * @brief For function my_atoi(), validate failure when non-numeric values of
+ *        selected base are in the input string.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__7(void **state)
   {
   char8_t buffer[] = "123x";
-  assert_int_equal(my_atoi((uint8_t*)buffer, 4, 16, NULL), FAILURE);
+  int32_t data;
+  assert_int_equal(my_atoi((uint8_t*)buffer, 4, 16, &data), FAILURE);
   }
 
-
+/**
+ * @brief For function my_atoi(), validate base 16 conversion.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__8(void **state)
   {
   int32_t data;
@@ -121,6 +217,13 @@ void my_atoi__8(void **state)
   assert_int_equal(data, -4096);
   }
 
+/**
+ * @brief For function my_atoi(), validate base 10 conversion.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_atoi__9(void **state)
   {
   int32_t data;
@@ -131,17 +234,40 @@ void my_atoi__9(void **state)
 
 /*---------------------------------------------------------------------------*/
 
+/**
+ * @brief For function big_to_little32(), validate arg 'data' with invalid
+ *        value NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void big_to_little32__1(void **state)
   {
   assert_int_equal(big_to_little32(NULL, 1), FAILURE);
   }
 
+/**
+ * @brief For function big_to_little32(), validate arg 'length' with invalid
+ *        size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void big_to_little32__2(void **state)
   {
   uint32_t data;
   assert_int_equal(big_to_little32(&data, 0), FAILURE);
   }
 
+/**
+ * @brief For function big_to_little32(), validate conversion of 1 value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void big_to_little32__3(void **state)
   {
   uint32_t data = 0x5D29CF37;
@@ -149,6 +275,14 @@ void big_to_little32__3(void **state)
   assert_int_equal(data, 0x37CF295D);
   }
 
+/**
+ * @brief For function big_to_little32(), validate conversion of multiple
+ *        values.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void big_to_little32__4(void **state)
   {
   uint32_t data[4] =
@@ -167,17 +301,40 @@ void big_to_little32__4(void **state)
 
 /*---------------------------------------------------------------------------*/
 
+/**
+ * @brief For function little_to_big32(), validate arg 'data' with invalid
+ *        value NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void little_to_big32__1(void **state)
   {
   assert_int_equal(little_to_big32(NULL, 1), FAILURE);
   }
 
+/**
+ * @brief For function little_to_big32(), validate arg 'length' with invalid
+ *        size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void little_to_big32__2(void **state)
   {
   uint32_t data;
   assert_int_equal(little_to_big32(&data, 0), FAILURE);
   }
 
+/**
+ * @brief For function little_to_big32(), validate conversion of 1 value.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void little_to_big32__3(void **state)
   {
   uint32_t data = 0x5D29CF37;
@@ -185,6 +342,14 @@ void little_to_big32__3(void **state)
   assert_int_equal(data, 0x37CF295D);
   }
 
+/**
+ * @brief For function little_to_big32(), validate conversion of multiple
+ *        values.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void little_to_big32__4(void **state)
   {
   uint32_t data[4] =
