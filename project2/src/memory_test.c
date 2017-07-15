19ed9b9eada782c8464d1bf91e2444a7c6df876a
Robert Blazewicz <blaze@cardoduplex.com>
2017-07-15 18:24:50 -0400

    Add comments for memory tests

--- project2/src/memory_test.c
+++ project2/src/memory_test.c
@@ -27,33 +27,70 @@
 #define TEST_MEMMOVE_LENGTH  (16)
 
 /*---------------------------------------------------------------------------*/
-/* my_memmove */
 
+/**
+ * @brief For function my_memmove(), validate arg 'src' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__1(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memmove(NULL, data, 1), NULL);
   }
 
+/**
+ * @brief For function my_memmove(), validate arg 'dst' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__2(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memmove(data, NULL, 1), NULL);
   }
 
+/**
+ * @brief For function my_memmove(), validate arg 'length' with invalid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__3(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memmove(data, data, 0), NULL);
   }
 
+/**
+ * @brief For function my_memmove(), validate arg 'length' with valid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__4(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memmove(data, data, 1), data);
   }
 
-/* Test no-overlap condition */
+/**
+ * @brief For function my_memmove(), validate memory move with no-overlap
+ *        condition.The approach is based on an open-coded test provided by
+ *        Alex Fosdick in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__5(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -72,7 +109,15 @@ void my_memmove__5(void **state)
     }
   }
 
-/* Test source in destination region overlap */
+/**
+ * @brief For function my_memmove(), validate memory move with source in
+ *        destination region overlap condition. The approach is based on an
+ *        open-coded test provided by Alex Fosdick in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__6(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -91,7 +136,15 @@ void my_memmove__6(void **state)
     }
   }
 
-/* Test destination in source region overlap */
+/**
+ * @brief For function my_memmove(), validate memory move with destination in
+ *        source region overlap condition. The approach is based on an
+ *        open-coded test provided by Alex Fosdick in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memmove__7(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -111,32 +164,69 @@ void my_memmove__7(void **state)
   }
 
 /*---------------------------------------------------------------------------*/
-/* my_memcpy */
 
+/**
+ * @brief For function my_memcpy(), validate arg 'src' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memcpy__1(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memcpy(NULL, data, 1), NULL);
   }
 
+/**
+ * @brief For function my_memcpy(), validate arg 'dst' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memcpy__2(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memcpy(data, NULL, 1), NULL);
   }
 
+/**
+ * @brief For function my_memcpy(), validate arg 'length' with invalid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memcpy__3(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memcpy(data, data, 0), NULL);
   }
 
+/**
+ * @brief For function my_memcpy(), validate arg 'length' with valid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memcpy__4(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memcpy(data, data, 1), data);
   }
 
+/**
+ * @brief For function my_memcpu(), validate memory copy. The approach is
+ *        based on an open-coded test provided by Alex Fosdick in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memcpy__5(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -156,25 +246,56 @@ void my_memcpy__5(void **state)
   }
 
 /*---------------------------------------------------------------------------*/
-/* my_memset */
 
+/**
+ * @brief For function my_memset(), validate arg 'src' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memset__1(void **state)
   {
   assert_int_equal(my_memset(NULL, 1, 0), NULL);
   }
 
+/**
+ * @brief For function my_memset(), validate arg 'length' with invalid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memset__2(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memset(data, 0, 0), NULL);
   }
 
+/**
+ * @brief For function my_memset(), validate setting a single target byte.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memset__3(void **state)
   {
-  uint8_t data[1];
-  assert_int_equal(my_memset(data, 1, 0), data);
+  uint8_t data[1] = { 0xFF };
+  assert_int_equal(my_memset(data, 1, 0xDD), data);
+  assert_int_equal(data[0], 0xDD);
   }
 
+/**
+ * @brief For function my_memset(), validate set and zeroing functionallity.
+ *        The approach is based on an open-coded test provided by Alex Fosdick
+ *        in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memset__4(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -198,25 +319,55 @@ void my_memset__4(void **state)
   }
 
 /*---------------------------------------------------------------------------*/
-/* my_memzero */
 
+/**
+ * @brief For function my_memzero(), validate arg 'src' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memzero__1(void **state)
   {
   assert_int_equal(my_memzero(NULL, 1), NULL);
   }
 
+/**
+ * @brief For function my_memzero(), validate arg 'length' with invalid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memzero__2(void **state)
   {
   uint8_t data[1];
   assert_int_equal(my_memzero(data, 0), NULL);
   }
 
+/**
+ * @brief For function my_memzero(), validate zeroing a single target byte.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memzero__3(void **state)
   {
-  uint8_t data[1];
+  uint8_t data[1] = { 0xFF };
   assert_int_equal(my_memzero(data, 1), data);
+  assert_int_equal(data[0], 0x00);
   }
 
+/**
+ * @brief For function my_memzero(), validate memory zeroing. The approach is
+ *        based on an open-coded test provided by Alex Fosdick in project 1.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_memzero__4(void **state)
   {
   uint8_t data[MEM_SET_SIZE_B] =
@@ -236,60 +387,133 @@ void my_memzero__4(void **state)
   }
 
 /*---------------------------------------------------------------------------*/
-/* my_reverse */
 
+/**
+ * @brief For function my_reverse(), validate arg 'src' with invalid value
+ *        NULL.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__1(void **state)
   {
   assert_int_equal(my_reverse(NULL, 0), NULL);
   }
 
+/**
+ * @brief For function my_reverse(), validate arg 'length' with invalid size.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__2(void **state)
   {
   assert_int_equal(my_reverse((uint8_t*)"abc", 0), NULL);
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 1 char.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__3(void **state)
   {
   uint8_t src[] = "a";
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src, 1), "a", 1), 0);
+  assert_string_equal((const char8_t*)my_reverse(src, 1), "a");
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 2 chars.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__4(void **state)
   {
   uint8_t src[] = "ab";
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src, 2), "ba", 2), 0);
+  assert_string_equal((const char8_t*)my_reverse(src, 2), "ba");
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 3 chars.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__5(void **state)
   {
   uint8_t src[] = "abc";
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src, 3), "cba", 3), 0);
+  assert_string_equal((const char8_t*)my_reverse(src, 3), "cba");
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 10 chars.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__6(void **state)
   {
   uint8_t src[] = "0123456789";
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src, 10), "9876543210", 10), 0);
+  assert_string_equal((const char8_t*)my_reverse(src, 10), "9876543210");
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 9 chars.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__7(void **state)
   {
   uint8_t src[] = "0123X6789";
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src, 9), "9876X3210", 9), 0);
+  assert_string_equal((const char8_t*)my_reverse(src, 9), "9876X3210");
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 10 chars and a
+ *        smaller length of only 5.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__8(void **state)
   {
   uint8_t src[] = "0123456789";
   assert_int_equal(strncmp((const char8_t*)my_reverse(src, 5), "4321056789", 5), 0);
   }
 
+/**
+ * @brief For function my_reverse(), validate a string of 6 chars and a larger
+ *        length of 6 against a longer string.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__9(void **state)
   {
   uint8_t src[] = "012345";
   assert_int_equal(strncmp((const char8_t*)my_reverse(src, 6), "5432106789", 6), 0);
   }
 
+/**
+ * @brief For function my_reverse(), validate reveral of a raw buffer with
+ *        values from 0 to 255.
+ *
+ * @param  state - Cmocka state handle
+ *
+ * @return Nothing
+ */
 void my_reverse__10(void **state)
   {
   uint8_t src1[256];
@@ -301,7 +525,11 @@ void my_reverse__10(void **state)
     src2[i] = 255 - i;
     }
 
-  assert_int_equal(strncmp((const char8_t*)my_reverse(src1, 256), (const char8_t*)src2, 256), 0);
+  assert_int_equal(my_reverse(src1, 256), src1);
+  for (size_t i = 0; i < 256; i++)
+    {
+    assert_int_equal(src1[i], src2[i]);
+    }
   }
 
 /*---------------------------------------------------------------------------*/
